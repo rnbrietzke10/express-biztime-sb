5c8d384afa982e90cf4e95e80a0c5f8813f97010
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-09-16 11:43:31 -0500

    add POST and PUT routes for companies

--- routes/companies.js
+++ routes/companies.js
@@ -12,22 +12,55 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/:code', async (req, res, next) => {
+  try {
+    const { code } = req.params;
+    const results = await db.query('SELECT * FROM companies WHERE code = $1', [
+      code,
+    ]);
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Can't find company with code of ${code}`, 404);
+    }
+    return res.json({ company: results.rows[0] });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { code, name, description } = req.body;
+    const results = await db.query(
+      'INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description',
+      [code, name, description]
+    );
+    return res.status(201).json({ company: results.rows[0] });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put('/:code', async (req, res, next) => {
+  try {
+    const { code } = req.params;
+    const { name, description } = req.body;
+    const results = await db.query(
+      `UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description;`,
+      [name, description, code]
+    );
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Can not update company with code ${code}`, 404);
+    }
+    return res.json({ company: results.rows[0] });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
 
 /** 
-GET /companies
-Returns list of companies, like {companies: [{code, name}, ...]}
-GET /companies/[code]
-Return obj of company: {company: {code, name, description}}
 
-If the company given cannot be found, this should return a 404 status response.
-
-POST /companies
-Adds a company.
-
-Needs to be given JSON like: {code, name, description}
-
-Returns obj of new company: {company: {code, name, description}}
 
 PUT /companies/[code]
 Edit existing company.
